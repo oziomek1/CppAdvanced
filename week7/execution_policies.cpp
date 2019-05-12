2ed1b8c5d58ca2aab47e0bfc0669d0340c74251f
oziomek1 <ozimekwojciech@zoho.eu>
2019-05-12 12:08:55 +0200

    week7

--- week7/execution_policies.cpp
+++ week7/execution_policies.cpp
@@ -2,24 +2,42 @@
 // Created by Wojciech Ozimek on 12/05/2019.
 //
 
-#include <vector>
 #include <algorithm>
-#include <numeric>
+#include <chrono>
 #include <execution>
-#include <mutex>
+#include <numeric>
+#include <iostream>
+#include <vector>
+
+// 1)
+
+template <typename F>
+void Runner(F func)
+{
+    auto start = std::chrono::steady_clock::now();
+    auto result = func();
+    auto stop = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration<double, std::milli>(stop - start).count() << " time in ms" << std::endl;
+}
 
 int main()
 {
-    std::vector<int> vec(10'000'000);
-    std::iota(vec.begin(), vec.end(), 0);
-    std::vector<int> output;
-    std::for_each(std::execution::par, vec.begin(), vec.end(),
-        [&output](int& elem)
-            {
-                if (elem % 2 == 0)
-                {
-                    output.push_back(elem);
-                }
-            }
-    );
+// 1)
+    std::vector<double> value(1000000, 1);
+
+// std::execution::sequenced_policy
+    Runner([&value] {
+        return std::reduce(std::execution::seq, value.begin(), value.end());
+    });
+
+// std::execution::parallel_policy
+    Runner([&value] {
+        return std::reduce(std::execution::par, value.begin(), value.end());
+    });
+
+// std::execution::parallel_unsequenced_policy
+    Runner([&value] {
+        return std::reduce(std::execution::par_unseq, value.begin(), value.end())
+    });
 }
